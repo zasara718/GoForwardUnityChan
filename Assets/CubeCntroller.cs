ae7829657aae5b1c0097585bbb10f3050cf6ca2b
zasara <zasara718@gmail.com>
2019-03-09 18:42:41 +0900

    Lesson7 課題
    キューブが地面とキューブ同士が接触するときに効果音がなるようにしました。

---
+++ Assets/CubeCntroller.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeCntroller : MonoBehaviour {
+
+	//キューブの移動速度
+	private float speed = -0.2f;
+
+	//消滅位置
+	private float deadLine = -10;
+
+	//キューブ音
+	private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent <AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//キューブを移動させる
+		transform.Translate (this.speed, 0, 0);
+
+		//画面外に出たら破棄する
+		if (transform.position.x < this.deadLine) {
+			Destroy (gameObject);
+		}
+	}
+
+	//キューブがグラウンドかキューブ同士と接触した時
+	void OnCollisionEnter2D (Collision2D other) {
+		//グラウンドタグに接触した時
+		if (other.gameObject.tag == "GroundTag") {
+				//音を鳴らす
+				audioSource.Play ();
+		}
+		//キューブタグに接触した時
+		if (other.gameObject.tag == "CubeTag") {
+				//音を鳴らす
+				audioSource.Play ();
+		}
+		
+	}
+}
---
+++ Assets/UIController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIController : MonoBehaviour {
+	//ゲームオーバーテキスト
+	private GameObject gameOverText;
+	//走行距離テキスト
+	private GameObject runLengthText;
+	//走った距離
+	private float len = 0;
+	//走る速度
+	private float speed = 0.03f;
+	//ゲームオーバー判定
+	private bool isGameOver = false;
+
+	// Use this for initialization
+	void Start () {
+		//シーンビューからオブジェクトの実体を検索する
+		this.gameOverText = GameObject.Find ("GameOver");
+		this.runLengthText = GameObject.Find ("RunLength");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.isGameOver == false) {
+			//走った距離を更新する
+			this.len += this.speed;
+			//走った距離を表示する
+			this.runLengthText.GetComponent<Text> ().text = "Distance:  " + len.ToString ("F2") + "m";
+		}
+
+		//ゲームオーバになった場合
+		if (this.isGameOver == true) {
+			//クリックされたらシーンをロードする
+			if (Input.GetMouseButtonDown (0)) {
+				//GameSceneを読み込む
+				SceneManager.LoadScene ("GameScene");
+			}
+		}
+	}
+
+	public void GameOver() {
+		//ゲームオーバーになった時画面上にゲームーオーバーを表示する
+		this.gameOverText.GetComponent<Text> ().text = "GameOver";
+		this.isGameOver = true;
+	}
+}
